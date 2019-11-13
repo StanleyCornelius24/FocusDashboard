40c773bbf12fef01a6c4b9dba04a6cb4aeab8f5c
StanleyCornelius24 <dns-admin@focusonline.co.za>
2019-11-13 14:18:19 +0200

    Buefy Table added

---
+++ src/components/ClientTableBue.vue
@@ -0,0 +1,40 @@
+<template>
+    <b-table :data="clients" :columns="columns"></b-table>
+</template>
+
+<script>
+    export default {
+        props: {
+          clients: Array,
+        },
+data() {
+return {
+columns: [
+{
+field: 'client.id',
+label: 'ID',
+},
+{
+field: 'client.clientName',
+label: 'Client Name',
+searchable: true,
+},
+{
+field: 'last_name',
+label: 'Last Name',
+searchable: true,
+},
+{
+field: 'date',
+label: 'Date',
+centered: true
+},
+{
+field: 'gender',
+label: 'Gender',
+}
+]
+}
+}
+}
+</script>
